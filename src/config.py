5c979c1ff73c465d833560fe651ea753713e77ac
sahilkumar15 <sahilkumar4ai@gmail.com>
2024-07-29 10:45:54 -0400

    config

--- src/config.py
+++ src/config.py
@@ -17,7 +17,7 @@ class KatzBotConfig:
             base_dir (str): The base directory path where the application resides.
         """
         
-        self.BASE_DIR = r'D:\Yeshiva\Spring24\AI\App_Katzbot\katzbot_app_2'
+        # self.BASE_DIR = r''
         self.load_environment_variables()
         self.setup_file_paths()
         self.load_api_keys()
@@ -28,12 +28,12 @@ class KatzBotConfig:
 
     def setup_file_paths(self):
         """Setup file paths for various resources required by the application."""
-        self.BOT_IMAGE_PATH = os.path.join(self.BASE_DIR, r'static/static/ai_icon.png')
-        self.HUMAN_IMAGE_PATH = os.path.join(self.BASE_DIR, r'static/static/user_icon.png')
-        self.CSS_PATH = os.path.join(self.BASE_DIR, r'static/static/styles.css')
+        self.BOT_IMAGE_PATH = r'static/static/ai_icon.png'
+        self.HUMAN_IMAGE_PATH = r'static/static/user_icon.png'
+        self.CSS_PATH = r'static/static/styles.css'
         # self.DATA_FILE = os.path.join(self.BASE_DIR, r'data/loaded_data_train_test_2.pkl')
-        self.DATA_FILE = os.path.join(self.BASE_DIR, r'data/loaded_dataset_website.pkl')
-        self.CHAT_HISTORY_FILE = os.path.join(self.BASE_DIR, r'session_chat_history.csv')
+        self.DATA_FILE = r'data/loaded_dataset_website.pkl'
+        self.CHAT_HISTORY_FILE = r'session_chat_history.csv'
         
         # self.model_choice=["Mixtral-8x7b-32768", "Gemma2-9b-It", "Gemma-7b-It", "Llama-3.1-70b-Versatile", "Llama-3.1-8b-Instant", "Llama3-70b-8192", "Llama3-8b-8192"]
         # self.model_name = self.model_choice[0]
